c7085a66e9cbf4520d8b3df01cf4fb9aa176085b
Petmadsh <peteremadsh9@gmail.com>
2024-07-03 18:53:22 +0200

    Update homepage.dart

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -11,11 +11,13 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-  List<QueryDocumentSnapshot> cities = [];
+  List<QueryDocumentSnapshot> citiesList = [];
   List<QueryDocumentSnapshot> categoriesList = [];
 
   CollectionReference categories =
-      FirebaseFirestore.instance.collection('categories');
+  FirebaseFirestore.instance.collection('categories');
+  CollectionReference cities =
+  FirebaseFirestore.instance.collection('cities');
 
   Future<void> addCategories() async {
     // Check if categories already exist to avoid duplicates
@@ -23,12 +25,12 @@ class _HomePageState extends State<HomePage> {
     if (querySnapshot.docs.isEmpty) {
       // Add categories if they do not exist
       await Future.wait([
-        categories.add(
-            {'image': 'images/categories/pharaonic.jpg', 'title': "Pharaonic"}),
-        categories
-            .add({'image': 'images/categories/coptic.jpg', 'title': "Coptic"}),
-        categories.add(
-            {'image': 'images/categories/islamic.png', 'title': "Islamic"}),
+        categories.add({
+          'image': 'images/categories/pharaonic.jpg',
+          'title': "Pharaonic"
+        }),
+        categories.add({'image': 'images/categories/coptic.jpg', 'title': "Coptic"}),
+        categories.add({'image': 'images/categories/islamic.png', 'title': "Islamic"}),
         categories.add({
           'image': 'images/categories/romans_greeks.jpg',
           'title': "Greek/Roman"
@@ -40,40 +42,77 @@ class _HomePageState extends State<HomePage> {
     }
   }
 
-  bool isLoading = true;
-
-  getCities() async {
-    QuerySnapshot querySnapshot = await FirebaseFirestore.instance
-        .collection("cities")
-        .orderBy("name")
-        .get();
-    cities = querySnapshot.docs;
-
-    isLoading = false;
-
-    setState(() {});
+  Future<void> addCities() async {
+    // Check if cities already exist to avoid duplicates
+    QuerySnapshot querySnapshot = await cities.get();
+    if (querySnapshot.docs.isEmpty) {
+      // Add cities if they do not exist
+      await Future.wait([
+        cities.add({
+          'image': 'images/cities/aswan.jpeg',
+          'name': "Aswan",
+          'description':
+          "Aswan, located in southern Egypt on the Nile River, is known for its tranquil beauty and historical significance."
+        }),
+        cities.add({
+          'image': 'images/cities/cairo.jpeg',
+          'name': "Cairo",
+          'description':
+          "Cairo, the sprawling capital of Egypt, is a city steeped in history and culture."
+        }),
+        cities.add({
+          'image': 'images/cities/giza.jpeg',
+          'name': "Giza",
+          'description':
+          "Giza, a city on the western outskirts of Cairo, is renowned worldwide for its ancient monuments."
+        }),
+        cities.add({
+          'image': 'images/cities/luxor.jpeg',
+          'name': "Luxor",
+          'description':
+          "Luxor, often referred to as the world's greatest open-air museum,located in southern Egypt renowned for its extraordinary ancient monuments."
+        }),
+        cities.add({
+          'image': 'images/cities/alexandria.jpeg',
+          'name': "Alexandria",
+          'description':
+          "Alexandria, Egypt's second-largest city, is a Mediterranean gem with a rich historical tapestry."
+        }),
+        // Add more cities here as needed
+      ]);
+      print("Cities Added");
+    } else {
+      print("Cities already exist");
+    }
   }
 
-  bool Loading=true;
+  bool isLoadingCities = true;
+  bool isLoadingCategories = true;
+
+  getCities() async {
+    QuerySnapshot querySnapshot =
+    await FirebaseFirestore.instance.collection("cities").orderBy("name").get();
+    setState(() {
+      citiesList = querySnapshot.docs;
+      isLoadingCities = false;
+    });
+  }
 
   getCategories() async {
     QuerySnapshot querySnapshot = await FirebaseFirestore.instance
         .collection("categories")
         .orderBy("title")
         .get();
-    categoriesList = querySnapshot.docs;
-    Loading = false;
-
-    setState(() {});
+    setState(() {
+      categoriesList = querySnapshot.docs;
+      isLoadingCategories = false;
+    });
   }
-
-
-
-
 
   @override
   void initState() {
     super.initState();
+    addCities();
     addCategories().then((_) {
       getCities();
       getCategories();
@@ -83,177 +122,167 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: Row(
-            children: [
-              Text("Homepage"),
-            ],
-          ),
-          actions: [
-            IconButton(
-                onPressed: () async {
-                  GoogleSignIn googleSignIn = GoogleSignIn();
-                  await googleSignIn.disconnect();
-                  await FirebaseAuth.instance.signOut();
-                  Navigator.of(context)
-                      .pushNamedAndRemoveUntil("login", (route) => false);
+      appBar: AppBar(
+        title: const Text("Homepage"),
+        actions: [
+          IconButton(
+            onPressed: () async {
+              GoogleSignIn googleSignIn = GoogleSignIn();
+              await googleSignIn.disconnect();
+              await FirebaseAuth.instance.signOut();
+              Navigator.of(context).pushNamedAndRemoveUntil("login", (route) => false);
+            },
+            icon: const Icon(Icons.exit_to_app),
+          )
+        ],
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        iconSize: 40,
+        selectedItemColor: Colors.orange,
+        items: const [
+          BottomNavigationBarItem(icon: Icon(Icons.home_outlined), label: ""),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.shopping_bag_outlined), label: ""),
+          BottomNavigationBarItem(icon: Icon(Icons.person_outline), label: ""),
+        ],
+      ),
+      body: Container(
+        padding: const EdgeInsets.all(20),
+        child: ListView(
+          children: [
+            Row(
+              children: [
+                Expanded(
+                  child: TextFormField(
+                    decoration: InputDecoration(
+                      prefixIcon: const Icon(Icons.search, size: 30),
+                      hintText: "Search",
+                      border: InputBorder.none,
+                      fillColor: Colors.grey[150],
+                      filled: true,
+                    ),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: const Icon(
+                    Icons.menu,
+                    size: 40,
+                  ),
+                )
+              ],
+            ),
+            const SizedBox(height: 30),
+            const Text(
+              "Categories",
+              style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+            ),
+            const SizedBox(height: 20),
+            isLoadingCategories
+                ? Center(child: CircularProgressIndicator())
+                : Container(
+              height: 100,
+              child: ListView.builder(
+                itemCount: categoriesList.length,
+                scrollDirection: Axis.horizontal,
+                itemBuilder: (BuildContext context, int index) {
+                  var category =
+                  categoriesList[index].data() as Map<String, dynamic>;
+                  return InkWell(
+                    onTap: () {
+                      // Navigate to category details if needed
+                    },
+                    child: Container(
+                      margin: const EdgeInsets.only(right: 10),
+                      child: Column(
+                        children: [
+                          Container(
+                            decoration: BoxDecoration(
+                              color: Colors.grey[300],
+                              borderRadius: BorderRadius.circular(100),
+                            ),
+                            padding: const EdgeInsets.all(5),
+                            child: ClipOval(
+                              child: Image.asset(
+                                category['image'],
+                                width: 70,
+                                height: 70,
+                                fit: BoxFit.cover,
+                              ),
+                            ),
+                          ),
+                          Text(
+                            category['title'],
+                            style:  TextStyle(
+                              fontWeight: FontWeight.bold,
+                              color: Colors.grey[800],
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  );
                 },
-                icon: Icon(Icons.exit_to_app))
+              ),
+            ),
+            const SizedBox(height: 30),
+            const Text(
+              "Cities",
+              style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+            ),
+            const SizedBox(height: 20),
+            isLoadingCities
+                ? Center(child: CircularProgressIndicator())
+                : GridView.builder(
+              itemCount: citiesList.length,
+              physics: const NeverScrollableScrollPhysics(),
+              shrinkWrap: true,
+              gridDelegate:
+              const SliverGridDelegateWithFixedCrossAxisCount(
+                crossAxisCount: 1,
+                mainAxisExtent: 280,
+              ),
+              itemBuilder: (BuildContext context, int index) {
+                var city = citiesList[index].data() as Map<String, dynamic>;
+                return InkWell(
+                  onTap: () {
+                    // Navigate to city details if needed
+                  },
+                  child: Padding(
+                    padding: const EdgeInsets.only(bottom: 15),
+                    child: Card(
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        children: [
+                          Container(
+                            width: 300,
+                            child: Image.asset(
+                              city['image'],
+                              height: 170,
+                              fit: BoxFit.fill,
+                            ),
+                          ),
+                          Text(
+                            city['name'],
+                            style: const TextStyle(
+                              fontWeight: FontWeight.bold,
+                              fontSize: 16,
+                            ),
+                          ),
+                          Text(
+                            city['description'],
+                            style: const TextStyle(color: Colors.grey),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                );
+              },
+            ),
           ],
         ),
-        bottomNavigationBar: BottomNavigationBar(
-            iconSize: 40,
-            selectedItemColor: Colors.orange,
-            items: [
-              BottomNavigationBarItem(
-                  icon: Icon(Icons.home_outlined), label: ""),
-              BottomNavigationBarItem(
-                  icon: Icon(Icons.shopping_bag_outlined), label: ""),
-              BottomNavigationBarItem(
-                  icon: Icon(Icons.person_outline), label: ""),
-            ]),
-        body: Container(
-          padding: const EdgeInsets.all(20),
-          child: ListView(
-            children: [
-              Row(
-                children: [
-                  Expanded(
-                      child: TextFormField(
-                    decoration: InputDecoration(
-                        prefixIcon: const Icon(
-                          Icons.search,
-                          size: 30,
-                        ),
-                        hintText: "Search",
-                        border: InputBorder.none,
-                        fillColor: Colors.grey[150],
-                        filled: true),
-                  )),
-                  const Padding(
-                    padding: EdgeInsets.all(8.0),
-                    child: Icon(
-                      Icons.menu,
-                      size: 40,
-                    ),
-                  )
-                ],
-              ),
-              Container(
-                height: 30,
-              ),
-              const Text(
-                "Categories",
-                style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
-              ),
-              Container(
-                height: 20,
-              ),
-              Loading
-                  ? Center(
-                child: CircularProgressIndicator(),
-              )
-                  :
-              Container(
-                height: 100,
-                child: ListView.builder(
-                  itemCount: categoriesList.length,
-                  scrollDirection: Axis.horizontal,
-                  itemBuilder: (BuildContext context, int index) {
-                    var category =
-                        categoriesList[index].data() as Map<String, dynamic>;
-                    return InkWell(
-                      onTap: () {
-                        // Navigator.of(context).push(MaterialPageRoute(builder: (context) => ItemsDetails(data:bestselling[index],)));
-                      },
-                      child: Container(
-                        margin: const EdgeInsets.only(right: 10),
-                        child: Column(
-                          children: [
-                            Container(
-                              decoration: BoxDecoration(
-                                  color: Colors.grey[300],
-                                  borderRadius: BorderRadius.circular(100)),
-                              padding: const EdgeInsets.all(5),
-                              child: ClipOval(
-                                child: Image.asset(
-                                  category['image'],
-                                  width: 70,
-                                  height: 70,
-                                  fit: BoxFit.cover,
-                                ),
-                              ),
-                            ),
-                            Text(
-                              category['title'],
-                              style: TextStyle(
-                                  fontWeight: FontWeight.bold,
-                                  color: Colors.grey[800]),
-                            ),
-                          ],
-                        ),
-                      ),
-                    );
-                  },
-                ),
-              ),
-              Container(
-                padding: const EdgeInsets.symmetric(vertical: 10),
-                child: const Text(
-                  "Cities",
-                  style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
-                ),
-              ),
-              isLoading
-                  ? Center(
-                      child: CircularProgressIndicator(),
-                    )
-                  : GridView.builder(
-                      itemCount: cities.length,
-                      physics: const NeverScrollableScrollPhysics(),
-                      shrinkWrap: true,
-                      gridDelegate:
-                          const SliverGridDelegateWithFixedCrossAxisCount(
-                              crossAxisCount: 1, mainAxisExtent: 280),
-                      itemBuilder: (BuildContext context, int index) {
-                        var city = cities[index].data() as Map<String, dynamic>;
-                        return InkWell(
-                            onTap: () {
-                              // Navigator.of(context).push(MaterialPageRoute(builder: (context) => ItemsDetails(data:bestselling[index],)));
-                            },
-                            child: Padding(
-                              padding: const EdgeInsets.only(bottom: 15),
-                              child: Card(
-                                child: Column(
-                                    crossAxisAlignment:
-                                        CrossAxisAlignment.center,
-                                    children: [
-                                      Container(
-                                        width: 300,
-                                        child: Image.asset(
-                                          city['image'],
-                                          height: 170,
-                                          fit: BoxFit.fill,
-                                        ),
-                                      ),
-                                      Text(
-                                        city['name'],
-                                        style: const TextStyle(
-                                            fontWeight: FontWeight.bold,
-                                            fontSize: 16),
-                                      ),
-                                      Text(
-                                        city['description'],
-                                        style:
-                                            const TextStyle(color: Colors.grey),
-                                      ),
-                                    ]),
-                              ),
-                            ));
-                      },
-                    ),
-            ],
-          ),
-        ));
+      ),
+    );
   }
 }
